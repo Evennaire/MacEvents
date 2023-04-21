841631e31b7d6d4cc7b73a7b836ee7a7644a3035
even <evenyin59@gmail.com>
2023-04-21 16:07:54 +0800

    init

---
+++ MacEvents/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacEvents
+//
+//  Created by Even on 2023/4/21.
+//
+
+import SwiftUI
+//import AxSwift
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacEvents/MacEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacEventsApp.swift
+//  MacEvents
+//
+//  Created by Even on 2023/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MacEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
